a5cbdcfa15c2634214eb4b3c56219aa32cc6664f
ZhangBiFen <ZhangBiFen@gmail.com>
2019-06-25 09:55:11 +0800

    Implement the net ineterface Xmit, Transaction pool and node infomation update to get inventory
    Reformat the code with the go fmt command to make code style clear

--- core/ledger/block.go
+++ core/ledger/block.go
@@ -7,7 +7,6 @@ import (
 	tx "dad-go/core/transaction"
 	"dad-go/crypto"
 	. "dad-go/errors"
-	msg "dad-go/net/message"
 	"io"
 )
 
@@ -91,8 +90,8 @@ func (b *Block) Verify() error {
 	return nil
 }
 
-func (b *Block) Type() msg.InventoryType {
-	return msg.BLOCK
+func (b *Block) Type() InventoryType {
+	return BLOCK
 }
 
 func (bc *Blockchain) GetBlock(height uint32) (*Block, error) {
