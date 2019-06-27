344fb6797bb97711ea3412d50ebfb61d036ec8e0
bitking0522 <bitking0522@gmail.com>
2019-06-27 10:59:38 +0800

    The node listen port was set static at source code, it is inconvninent when running more than 1 node in one PC due to port conflict, here we enable them be configurated at file and don't need to modify the source code.

--- net/node/link.go
+++ net/node/link.go
@@ -4,6 +4,7 @@ import (
 	"dad-go/common"
 	. "dad-go/net/message"
 	. "dad-go/net/protocol"
+	. "dad-go/config"
 	"errors"
 	"fmt"
 	"io"
@@ -107,7 +108,7 @@ func printIPAddr() {
 // Init the server port, should be run in another thread
 func (n *node) initConnection() {
 	common.Trace()
-	listener, err := net.Listen("tcp", ":"+strconv.Itoa(NODETESTPORT))
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(Parameters.NodePort))
 	if err != nil {
 		fmt.Println("Error listening\n", err.Error())
 		return
