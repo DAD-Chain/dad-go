7da3500eb9db9c54d018aeff637c818b1a62ceff
genesisofheroes <genesisofheroes@gmail.com>
2019-07-02 14:46:08 +0800

    It should be blocksreq not blockreq

--- net/message/message.go
+++ net/message/message.go
@@ -1,15 +1,15 @@
 package message
 
 import (
-	"github.com/DAD-Chain/dad-go/common"
-	"github.com/DAD-Chain/dad-go/common/log"
-	. "github.com/DAD-Chain/dad-go/net/protocol"
 	"bytes"
 	"crypto/sha256"
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"github.com/DAD-Chain/dad-go/common"
+	"github.com/DAD-Chain/dad-go/common/log"
+	. "github.com/DAD-Chain/dad-go/net/protocol"
 )
 
 type Messager interface {
@@ -130,7 +130,7 @@ func AllocMsg(t string, length int) Messager {
 		copy(msg.msgHdr.CMD[0:len(t)], t)
 		return &msg
 	case "getblocks":
-		var msg blockReq
+		var msg blocksReq
 		copy(msg.msgHdr.CMD[0:len(t)], t)
 		return &msg
 	case "txnpool":
