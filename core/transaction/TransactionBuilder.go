3a9c59cae7337043c8ab4bc1fae51d7ec421f450
genesisofheroes <genesisofheroes@gmail.com>
2019-07-29 10:21:21 +0800

    iThere have problem with issue and transfer ,can't normal complete. fixed the grpc tx generate and transaction building funcs.

--- core/transaction/TransactionBuilder.go
+++ core/transaction/TransactionBuilder.go
@@ -1,85 +1,64 @@
 package transaction
 
-
 import (
-	"dad-go/core/asset"
 	"dad-go/common"
-	"dad-go/crypto"
-	"dad-go/core/transaction/payload"
+	"dad-go/core/asset"
 	"dad-go/core/contract/program"
+	"dad-go/core/transaction/payload"
+	"dad-go/crypto"
 )
 
 //initial a new transaction with asset registration payload
-func NewRegisterAssetTransaction(asset *asset.Asset,amount common.Fixed64,issuer *crypto.PubKey,conroller common.Uint160) (*Transaction, error){
+func NewRegisterAssetTransaction(asset *asset.Asset, amount common.Fixed64, issuer *crypto.PubKey, conroller common.Uint160) (*Transaction, error) {
 
 	//TODO: check arguments
 
-	assetRegPayload := &payload.RegisterAsset {
-		Asset: asset,
+	assetRegPayload := &payload.RegisterAsset{
+		Asset:  asset,
 		Amount: amount,
 		//Precision: precision,
-		Issuer: issuer,
+		Issuer:     issuer,
 		Controller: conroller,
 	}
 
-    return &Transaction{
-        //nonce uint64 //TODO: genenrate nonce
-        UTXOInputs: []*UTXOTxInput{},
-        BalanceInputs: []*BalanceTxInput{},
-        Attributes: []*TxAttribute{},
-        TxType: RegisterAsset,
-        Payload: assetRegPayload,
-        Programs: []*program.Program{},
-    }, nil
-}
-
-func NewIssueAssetTransaction() (*Transaction, error){
-
-    assetRegPayload := &payload.IssueAsset {
-    }
-
-    return &Transaction{
-        UTXOInputs: []*UTXOTxInput{},
-        BalanceInputs: []*BalanceTxInput{},
-        Attributes: []*TxAttribute{},
-        TxType: IssueAsset,
-        Payload: assetRegPayload,
-        Programs: []*program.Program{},
-    }, nil
-}
-
-func NewTransferAssetTransaction() (*Transaction, error){
-
-    //TODO: check arguments
-
-    assetRegPayload := &payload.TransferAsset {
-    }
-
-    return &Transaction{
-        UTXOInputs: []*UTXOTxInput{},
-        BalanceInputs: []*BalanceTxInput{},
-        Attributes: []*TxAttribute{},
-        TxType: TransferAsset,
-        Payload: assetRegPayload,
-        Programs: []*program.Program{},
-    }, nil
-}
-
-//initial a new transaction with record payload
-func NewRecordTransaction(recordType string, recordData []byte) (*Transaction, error) {
-
-	//TODO: check arguments
-	recordPayload := &payload.Record{
-		RecordType: recordType,
-		RecordData: recordData,
-	}
-
 	return &Transaction{
-		TxType:        Record,
-		Payload:       recordPayload,
-		Attributes:    []*TxAttribute{},
+		//nonce uint64 //TODO: genenrate nonce
 		UTXOInputs:    []*UTXOTxInput{},
 		BalanceInputs: []*BalanceTxInput{},
+		Attributes:    []*TxAttribute{},
+		TxType:        RegisterAsset,
+		Payload:       assetRegPayload,
+		Programs:      []*program.Program{},
+	}, nil
+}
+
+func NewIssueAssetTransaction(outputs []*TxOutput) (*Transaction, error) {
+
+	assetRegPayload := &payload.IssueAsset{}
+
+	return &Transaction{
+		TxType:        IssueAsset,
+		Payload:       assetRegPayload,
+		Attributes:    []*TxAttribute{},
+		BalanceInputs: []*BalanceTxInput{},
+		Outputs:       outputs,
+		Programs:      []*program.Program{},
+	}, nil
+}
+
+func NewTransferAssetTransaction(inputs []*UTXOTxInput, outputs []*TxOutput) (*Transaction, error) {
+
+	//TODO: check arguments
+
+	assetRegPayload := &payload.TransferAsset{}
+
+	return &Transaction{
+		TxType:        TransferAsset,
+		Payload:       assetRegPayload,
+		Attributes:    []*TxAttribute{},
+		UTXOInputs:    inputs,
+		BalanceInputs: []*BalanceTxInput{},
+		Outputs:       outputs,
 		Programs:      []*program.Program{},
 	}, nil
 }
