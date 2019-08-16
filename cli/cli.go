ff0606b80d27be9bd72a020e7799c8c60a61e272
jonewillim00 <jonewillim00@gmail.com>
2019-08-16 09:21:21 +0800

    reorganize command line tool nodectl with urfave/cli framework

---
+++ cli/cli.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"os"
+	"sort"
+
+	. "dad-go/cli/common"
+	"dad-go/cli/consensus"
+	"dad-go/cli/debug"
+	"dad-go/cli/info"
+	"dad-go/cli/test"
+
+	"github.com/urfave/cli"
+)
+
+func init() {
+	app := cli.NewApp()
+	app.Name = "nodectl"
+	app.Version = "1.0.1"
+	app.HelpName = "nodectl"
+	app.Usage = "command line tool for dad-go blockchain"
+	app.UsageText = "nodectl [global options] command [command options] [args]"
+	app.HideHelp = false
+	app.HideVersion = false
+	//global options
+	app.Flags = []cli.Flag{
+		NewIpFlag(),
+		NewPortFlag(),
+	}
+	//commands
+	app.Commands = []cli.Command{
+		*consensus.NewCommand(),
+		*debug.NewCommand(),
+		*info.NewCommand(),
+		*test.NewCommand(),
+	}
+	sort.Sort(cli.CommandsByName(app.Commands))
+	sort.Sort(cli.FlagsByName(app.Flags))
+
+	app.Run(os.Args)
+}
