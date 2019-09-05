d30ec7d5e4f9ac07f5bd9068e89a535bb6800f7b
jonewillim00 <jonewillim00@gmail.com>
2019-09-05 18:33:25 +0800

    Need to input wallet password when node run
    bring in howeyc/gopass to get the input password

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"dad-go/common/config"
 	"dad-go/account"
+	"dad-go/common/config"
 	"dad-go/common/log"
 	"dad-go/consensus/dbft"
 	"dad-go/core/ledger"
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	DefaultMultiCoreNum    = 4
+	DefaultMultiCoreNum = 4
 )
 
 func init() {
@@ -26,7 +26,7 @@ func init() {
 	log.CreatePrintLog(path)
 
 	var coreNum int
-	if (config.Parameters.MultiCoreNum > DefaultMultiCoreNum) {
+	if config.Parameters.MultiCoreNum > DefaultMultiCoreNum {
 		coreNum = int(config.Parameters.MultiCoreNum)
 	} else {
 		coreNum = DefaultMultiCoreNum
@@ -36,7 +36,7 @@ func init() {
 }
 
 func main() {
-	var  acct *account.Account
+	var acct *account.Account
 	var blockChain *ledger.Blockchain
 	var err error
 	var noder protocol.Noder
