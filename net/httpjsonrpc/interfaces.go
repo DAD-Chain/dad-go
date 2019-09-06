c75a832d4c4bb9cdf9e34594d6e198e16fb3ff11
zhangfengzee <zhangfengzee@gmail.com>
2019-09-06 09:17:51 +0800

    add bookkeeper transaction logic  in ledger store module and payload validation

--- net/httpjsonrpc/interfaces.go
+++ net/httpjsonrpc/interfaces.go
@@ -491,14 +491,14 @@ func sendSampleTransaction(params []interface{}) map[string]interface{} {
 			return dad-goRpcInvalidParameter
 		}
 
-		walletFile := "wallet" + strconv.Itoa(ind) + ".txt"
-		c := client.OpenClient(walletFile, []byte("\x12\x34\x56"))
+		walletFile := "wallet" + strconv.Itoa(ind) + ".dat"
+		c := account.Open(walletFile, []byte(account.DefaultPin))
 		if c == nil {
-			return dad-goRpc("do not have wallet file" + walletFile)
+			return dad-goRpc("do not have wallet file:" + walletFile)
 		}
 
 		account, _ := c.GetDefaultAccount()
-		pubKey := account.PublicKey
+		pubKey := account.PubKey()
 
 		cert := make([]byte, 100)
 		rand.Read(cert)
