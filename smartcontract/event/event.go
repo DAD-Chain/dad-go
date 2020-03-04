feb1d943c4e7284ce2437448ead90aaa111e46f2
bitking0522 <bitking0522@gmail.com>
2020-03-04 11:22:27 +0800

    update smartcode event push

--- smartcontract/event/event.go
+++ smartcontract/event/event.go
@@ -1,1 +1,17 @@
 package event
+
+import (
+	"github.com/dad-go/events"
+	"github.com/dad-go/core/ledger"
+	. "github.com/dad-go/common"
+)
+
+func PushSmartCodeEvent(txHash Uint256, errcode int64, action string, result interface{}) {
+	resp := map[string]interface{}{
+		"TxHash": ToHexString(txHash.ToArrayReverse()),
+		"Action": action,
+		"Result": result,
+		"Error":  errcode,
+	}
+	ledger.DefaultLedger.Blockchain.BCEvents.Notify(events.EventSmartCode, resp)
+}
