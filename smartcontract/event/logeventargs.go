f18cf47bc0d5b666be263353fe5b08a65c3c2b95
ZhangBiFen <ZhangBiFen@gmail.com>
2020-03-23 18:33:24 +0800

    Add event return support
    (cherry picked from commit bbf89d6)

--- smartcontract/event/logeventargs.go
+++ smartcontract/event/logeventargs.go
@@ -1,12 +1,11 @@
 package event
 
 import (
-	"github.com/dad-go/vm/neovm/interfaces"
 	"github.com/dad-go/common"
 )
 
 type LogEventArgs struct {
-	container interfaces.ICodeContainer
-	codeHash  common.Uint160
-	message   string
+	Container common.Uint256
+	CodeHash  common.Uint160
+	Message   string
 }
