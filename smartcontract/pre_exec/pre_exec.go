97898dac0eb33260197f8b142ed377a022942f99
JhoneLee6 <JhoneLee6@gmail.com>
2020-04-06 08:40:05 +0800

    Update newstatestore function

--- smartcontract/pre_exec/pre_exec.go
+++ smartcontract/pre_exec/pre_exec.go
@@ -4,11 +4,11 @@ import (
 	"github.com/dad-go/smartcontract/service"
 	"github.com/dad-go/vm/neovm"
 	"github.com/dad-go/vm/neovm/interfaces"
-	"github.com/dad-go/core/store/statestore"
 	"github.com/dad-go/smartcontract/types"
 	"github.com/dad-go/core/store/ChainStore"
-	"github.com/dad-go/core/ledger"
 	"github.com/dad-go/smartcontract/common"
+	"github.com/dad-go/core/ledger"
+	"github.com/dad-go/core/store/statestore"
 	. "github.com/dad-go/common"
 )
 
@@ -20,7 +20,8 @@ func PreExec(code []byte, container interfaces.ICodeContainer) ([]interface{}, e
 		err error
 	)
 	crypto = new(neovm.ECDsaCrypto)
-	stateStore := ChainStore.NewStateStore(statestore.NewMemDatabase(), ledger.DefaultLedger.Store.(*ChainStore.ChainStore), statestore.NewTrieStore(nil), Uint256{})
+
+	stateStore := ChainStore.NewStateStore(statestore.NewMemDatabase(), ledger.DefaultLedger.Store.(*ChainStore.ChainStore), Uint256{})
 	stateMachine := service.NewStateMachine(stateStore, types.Application, nil)
 	se := neovm.NewExecutionEngine(container, crypto, ChainStore.NewCacheCodeTable(stateStore), stateMachine)
 	se.LoadCode(code, false)
