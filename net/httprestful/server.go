845b808eb4598404bc2eaf9ee711cec09d3025a2
windy0522 <windy0522@gmail.com>
2020-04-23 09:34:26 +0800

    delete some interface in restful and websocket

--- net/httprestful/server.go
+++ net/httprestful/server.go
@@ -1,37 +1,16 @@
 package httprestful
 
 import (
-	. "github.com/dad-go/common/config"
-	"github.com/dad-go/core/ledger"
-	"github.com/dad-go/events"
 	"github.com/dad-go/net/httprestful/common"
 	. "github.com/dad-go/net/httprestful/restful"
 	. "github.com/dad-go/net/protocol"
-	"strconv"
 )
 
 func StartServer(n Noder) {
 	common.SetNode(n)
-	ledger.DefaultLedger.Blockchain.BCEvents.Subscribe(events.EventBlockPersistCompleted, SendBlock2NoticeServer)
 	func() {
-		rest := InitRestServer(common.CheckAccessToken)
+		rest := InitRestServer()
 		go rest.Start()
 	}()
 }
 
-func SendBlock2NoticeServer(v interface{}) {
-
-	if len(Parameters.NoticeServerUrl) == 0 || !common.CheckPushBlock() {
-		return
-	}
-	go func() {
-		req := make(map[string]interface{})
-		req["Height"] = strconv.FormatInt(int64(ledger.DefaultLedger.Blockchain.BlockHeight), 10)
-		req = common.GetBlockByHeight(req)
-
-		repMsg, _ := common.PostRequest(req, Parameters.NoticeServerUrl)
-		if repMsg[""] == nil {
-			//TODO
-		}
-	}()
-}
