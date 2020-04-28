89d4319474dbb3cd1512064d445d21d3d0e6086e
genesisofheroes <genesisofheroes@gmail.com>
2020-04-28 11:46:33 +0800

    fix compile error in net.actor

--- net/actor/consensus.go
+++ net/actor/consensus.go
@@ -1,13 +1,12 @@
 package actor
 
 import (
-	"github.com/ONTID/eventbus/actor"
+	"github.com/dad-go/eventbus/actor"
 	//"github.com/dad-go/net/message"
 )
 
-var ConsensusPid actor.PID
+var ConsensusPid *actor.PID
 
 //func PushConsensus(cons *message.ConsensusPayload){
 //	ConsensusPid.Tell(cons)
 //}
-
