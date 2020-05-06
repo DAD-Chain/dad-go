edb224ba0fa11f11a67179a832f05580699b509a
genesisofheroes <genesisofheroes@gmail.com>
2020-05-07 06:32:58 +0800

    Fix the issue about AddHeaders panic in net module.
    1.Fix the issue about AddHeaders panic in net module.
    2.Some other minor modifications

--- net/protocol/protocol.go
+++ net/protocol/protocol.go
@@ -3,6 +3,8 @@ package protocol
 import (
 	"bytes"
 	"encoding/binary"
+	"net"
+
 	"github.com/dad-go/common"
 	"github.com/dad-go/crypto"
 	"github.com/dad-go/events"
@@ -90,6 +92,7 @@ type Noder interface {
 	UpdateRXTime(t time.Time)
 	LocalNode() Noder
 	DelNbrNode(id uint64) (Noder, bool)
+	GetNbrNode(id uint64) (Noder, bool)
 	AddNbrNode(Noder)
 	CloseConn()
 	GetHeight() uint64
@@ -144,6 +147,8 @@ type Noder interface {
 	GetConsensusState() uint32
 	SetConsensusState(state uint32)
 	CloseConsensusConn()
+	SetConsensusConn(conn net.Conn)
+	GetConsensusConn() net.Conn
 }
 
 func (msg *NodeAddr) Deserialization(p []byte) error {
