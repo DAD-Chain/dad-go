68d7ec8c5e113c84dbae5e4e3fa4de46efd76e0e
winnerKim0304 <winnerKim0304@gmail.com>
2020-06-06 18:12:12 +0800

    unified naming rules

--- smartcontract/event/event.go
+++ smartcontract/event/event.go
@@ -19,15 +19,15 @@
 package event
 
 import (
-	. "github.com/dad-go/common"
+	"github.com/dad-go/common"
 	"github.com/dad-go/events"
 	"github.com/dad-go/events/message"
 	"github.com/dad-go/core/types"
 )
 
-func PushSmartCodeEvent(txHash Uint256, errcode int64, action string, result interface{}) {
+func PushSmartCodeEvent(txHash common.Uint256, errcode int64, action string, result interface{}) {
 	smartCodeEvt := &types.SmartCodeEvent{
-		TxHash: ToHexString(txHash.ToArray()),
+		TxHash: common.ToHexString(txHash.ToArray()),
 		Action: action,
 		Result: result,
 		Error:  errcode,
