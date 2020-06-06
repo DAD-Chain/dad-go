7e319c8b3bbb2970eaca0937b741ed6e7e496cac
winnerKim0304 <winnerKim0304@gmail.com>
2020-06-06 18:12:12 +0800

    unified naming rules

---
+++ vm/neovm/stack_item.go
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018 The dad-go Authors
+ * This file is part of The dad-go library.
+ *
+ * The dad-go is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The dad-go is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The dad-go.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neovm
+
+import "github.com/dad-go/vm/neovm/types"
+
+type StackItem struct {
+	_object types.StackItems
+}
+
+func NewStackItem(object types.StackItems) *StackItem {
+	var stackItem StackItem
+	stackItem._object = object
+	return &stackItem
+}
+
+func (s *StackItem) GetStackItem() types.StackItems {
+	return s._object
+}
+
+func (s *StackItem) GetExecutionContext() *ExecutionContext {
+	return nil
+}
