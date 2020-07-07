d2a122cda6f4318aa51b3d7fa7c92c3b40223560
ZhangBiFen <ZhangBiFen@gmail.com>
2020-07-07 08:52:25 +0800

    Add NeoVM Unit Test Case (#54)
    * neovm unit test
    
    * add NeoVM func stack unit test
    
    * add NeoVM slice unit test
    
    * add NeoVM bitwise unit test
    
    * fix struct clone bug, add neovm array unit test case
    
    * add NeoVM arithmetic unit test
    
    * refactor NeoVM unit test
    
    * add license

---
+++ vm/neovm/func_bitwise_test.go
@@ -0,0 +1,51 @@
+// Copyright 2017 The dad-go Authors
+// This file is part of the dad-go library.
+//
+// The dad-go library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The dad-go library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the dad-go library. If not, see <http://www.gnu.org/licenses/>.
+
+package neovm
+
+import (
+	"math/big"
+	"testing"
+
+	vtypes "github.com/ontio/dad-go/vm/neovm/types"
+)
+
+func TestOpInvert(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(123456789))))
+	e.EvaluationStack = stack
+
+	opInvert(&e)
+	i := big.NewInt(123456789)
+
+	if PeekBigInteger(&e).Cmp(i.Not(i)) != 0 {
+		t.Fatal("NeoVM OpInvert test failed.")
+	}
+}
+
+func TestOpEqual(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(123456789))))
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(123456789))))
+	e.EvaluationStack = stack
+
+	opEqual(&e)
+	if !PopBoolean(&e) {
+		t.Fatal("NeoVM OpEqual test failed.")
+	}
+}
--- vm/neovm/func_splice_test.go
+++ vm/neovm/func_splice_test.go
@@ -1,29 +1,90 @@
-/*
- * Copyright (C) 2018 The dad-go Authors
- * This file is part of The dad-go library.
- *
- * The dad-go is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The dad-go is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The dad-go.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2017 The dad-go Authors
+// This file is part of the dad-go library.
+//
+// The dad-go library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The dad-go library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the dad-go library. If not, see <http://www.gnu.org/licenses/>.
 
 package neovm
 
-import "testing"
+import (
+	"bytes"
+	"math/big"
+	"testing"
 
-func TestCat(t *testing.T) {
-	a := []byte{1, 2, 3, 4, 5, 6, 7, 8}
-	b := a[0:3]
-	c := []byte{7, 8}
-	d := append(b, c...)
-	t.Log("d", d)
+	vtypes "github.com/ontio/dad-go/vm/neovm/types"
+)
+
+func TestOpCat(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewByteArray([]byte("aaa"))))
+	stack.Push(NewStackItem(vtypes.NewByteArray([]byte("bbb"))))
+	e.EvaluationStack = stack
+
+	opCat(&e)
+	if Count(&e) != 1 || !bytes.Equal(PeekNByteArray(0, &e), []byte("aaabbb")) {
+		t.Fatalf("NeoVM OpCat test failed, expect aaabbb, got %s.", string(PeekNByteArray(0, &e)))
+	}
+}
+
+func TestOpSubStr(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewByteArray([]byte("12345"))))
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(1))))
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(4))))
+	e.EvaluationStack = stack
+
+	opSubStr(&e)
+	if !bytes.Equal(PeekNByteArray(0, &e), []byte("2345")) {
+		t.Fatalf("NeoVM OpSubStr test failed, expect 234, got %s.", string(PeekNByteArray(0, &e)))
+	}
+}
+
+func TestOpLeft(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewByteArray([]byte("12345"))))
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(4))))
+	e.EvaluationStack = stack
+
+	opLeft(&e)
+	if !bytes.Equal(PeekNByteArray(0, &e), []byte("1234")) {
+		t.Fatalf("NeoVM OpLeft test failed, expect 1234, got %s.", string(PeekNByteArray(0, &e)))
+	}
+}
+
+func TestOpRight(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewByteArray([]byte("12345"))))
+	stack.Push(NewStackItem(vtypes.NewInteger(big.NewInt(3))))
+	e.EvaluationStack = stack
+
+	opRight(&e)
+	if !bytes.Equal(PeekNByteArray(0, &e), []byte("345")) {
+		t.Fatalf("NeoVM OpRight test failed, expect 345, got %s.", string(PeekNByteArray(0, &e)))
+	}
+}
+
+func TestOpSize(t *testing.T) {
+	var e ExecutionEngine
+	stack := NewRandAccessStack()
+	stack.Push(NewStackItem(vtypes.NewByteArray([]byte("12345"))))
+	e.EvaluationStack = stack
+
+	opSize(&e)
+	if PeekInt(&e) != 5 {
+		t.Fatalf("NeoVM OpSize test failed, expect 5, got %d.", PeekInt(&e))
+	}
 }
