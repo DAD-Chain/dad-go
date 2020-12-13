8ff626da7f5a09a3a8b701522090caae8aaa450b
zhangfengzee <zhangfengzee@gmail.com>
2020-12-13 08:19:57 +0800

    ONT-604 refactor the auth contract (#207)
    * refactor the storage model of auth contract
    
    * re implement the auth contract based on new storage model
    
    * complete the refactor of auth contract
    
    * modify the param deserialize to be compatible with standard
    
    * change the name of storage key concat functions
    
    * change the type of field in packKeys

--- smartcontract/service/native/auth/state.go
+++ smartcontract/service/native/auth/state.go
@@ -1,315 +1,79 @@
+/*
+ * Copyright (C) 2018 The dad-go Authors
+ * This file is part of The dad-go library.
+ *
+ * The dad-go is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The dad-go is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The dad-go.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package auth
 
 import (
-	"bytes"
-	"github.com/ontio/dad-go/common/serialization"
-	"github.com/ontio/dad-go/errors"
 	"io"
-	"math"
+
+	"github.com/ontio/dad-go/common/serialization"
 )
 
-/* **********************************************   */
-type InitContractAdminParam struct {
-	AdminOntID []byte
+/*
+ * each role is assigned an array of funcNames
+ */
+type roleFuncs struct {
+	funcNames []string
 }
 
-func (this *InitContractAdminParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.AdminOntID); err != nil {
+func (this *roleFuncs) Serialize(w io.Writer) error {
+	if err := serialization.WriteUint32(w, uint32(len(this.funcNames))); err != nil {
 		return err
 	}
-	return nil
-}
-
-func (this *InitContractAdminParam) Deserialize(rd io.Reader) error {
-	var err error
-	if this.AdminOntID, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	return nil
-}
-
-/* **********************************************   */
-type TransferParam struct {
-	ContractAddr  []byte
-	NewAdminOntID []byte
-}
-
-func (this *TransferParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.ContractAddr); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.NewAdminOntID); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (this *TransferParam) Deserialize(rd io.Reader) error {
-	var err error
-	if this.ContractAddr, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.NewAdminOntID, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	return nil
-}
-
-/* **********************************************   */
-type FuncsToRoleParam struct {
-	ContractAddr []byte
-	AdminOntID   []byte
-	Role         []byte
-	FuncNames    []string
-}
-
-func (this *FuncsToRoleParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.ContractAddr); err != nil {
-		return errors.NewDetailErr(err, errors.ErrNoCode, "[FuncsToRoleParam] serialize contractAddr error")
-	}
-	if err := serialization.WriteVarBytes(w, this.AdminOntID); err != nil {
-		return errors.NewDetailErr(err, errors.ErrNoCode, "[FuncsToRoleParam] serialize adminOntID error")
-	}
-	if err := serialization.WriteVarBytes(w, this.Role); err != nil {
-		return errors.NewDetailErr(err, errors.ErrNoCode, "[FuncsToRoleParam] serialize role error")
-	}
-	if err := serialization.WriteVarUint(w, uint64(len(this.FuncNames))); err != nil {
-		return errors.NewDetailErr(err, errors.ErrNoCode, "[FuncsToRoleParam] serialize funcNames length error")
-	}
-	for _, fn := range this.FuncNames {
+	for _, fn := range this.funcNames {
 		if err := serialization.WriteString(w, fn); err != nil {
-			return errors.NewDetailErr(err, errors.ErrNoCode, "[FuncsToRoleParam] serialize funcNames error!")
+			return err
 		}
 	}
 	return nil
 }
 
-func (this *FuncsToRoleParam) Deserialize(rd io.Reader) error {
+func (this *roleFuncs) Deserialize(rd io.Reader) error {
 	var err error
-	var fnLen uint64
-	var i uint64
-
-	if this.ContractAddr, err = serialization.ReadVarBytes(rd); err != nil {
+	fnLen, err := serialization.ReadUint32(rd)
+	if err != nil {
 		return err
 	}
-	if this.AdminOntID, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Role, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if fnLen, err = serialization.ReadVarUint(rd, 0); err != nil {
-		return err
-	}
-	this.FuncNames = make([]string, fnLen)
-	for i = 0; i < fnLen; i++ {
+	this.funcNames = make([]string, 0)
+	for i := uint32(0); i < fnLen; i++ {
 		fn, err := serialization.ReadString(rd)
 		if err != nil {
 			return err
 		}
-		this.FuncNames[i] = fn
-	}
-	return nil
-}
-
-type OntIDsToRoleParam struct {
-	ContractAddr []byte
-	AdminOntID   []byte
-	Role         []byte
-	Persons      [][]byte
-}
-
-func (this *OntIDsToRoleParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.ContractAddr); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.AdminOntID); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Role); err != nil {
-		return errors.NewDetailErr(err, errors.ErrNoCode, "[PersonsToRole] serialize role error")
-	}
-	if err := serialization.WriteVarUint(w, uint64(len(this.Persons))); err != nil {
-		return errors.NewDetailErr(err, errors.ErrNoCode, "[PersonsToRole] serialize persons length error")
-	}
-	for _, p := range this.Persons {
-		if err := serialization.WriteVarBytes(w, p); err != nil {
-			return errors.NewDetailErr(err, errors.ErrNoCode, "[PersonsToRole] serialize persons error!")
-		}
-	}
-	return nil
-}
-
-func (this *OntIDsToRoleParam) Deserialize(rd io.Reader) error {
-	var err error
-	var pLen uint64
-	if this.ContractAddr, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.AdminOntID, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Role, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if pLen, err = serialization.ReadVarUint(rd, 0); err != nil {
-		return err
-	}
-	this.Persons = make([][]byte, pLen)
-	for i := uint64(0); i < pLen; i++ {
-		p, err := serialization.ReadVarBytes(rd)
-		if err != nil {
-			return err
-		}
-		this.Persons[i] = p
-	}
-	return nil
-}
-
-type DelegateParam struct {
-	ContractAddr []byte
-	From         []byte
-	To           []byte
-	Role         []byte
-	Period       uint32
-	Level        int
-}
-
-func (this *DelegateParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.ContractAddr); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.From); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.To); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Role); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarUint(w, uint64(this.Period)); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarUint(w, uint64(this.Level)); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (this *DelegateParam) Deserialize(rd io.Reader) error {
-	var err error
-	var period uint64
-	var level uint64
-	if this.ContractAddr, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.From, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.To, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Role, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if period, err = serialization.ReadVarUint(rd, 0); err != nil {
-		return err
-	}
-	if level, err = serialization.ReadVarUint(rd, 0); err != nil {
-		return err
-	}
-	if period > math.MaxUint32 || level > math.MaxInt8 {
-		return errors.NewErr("[auth contract] delegate deserialize: period or level too large")
-	}
-	this.Period = uint32(period)
-	this.Level = int(level)
-	return nil
-}
-
-type WithdrawParam struct {
-	ContractAddr []byte
-	Initiator    []byte
-	Delegate     []byte
-	Role         []byte
-}
-
-func (this *WithdrawParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.ContractAddr); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Initiator); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Delegate); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Role); err != nil {
-		return err
-	}
-	return nil
-}
-func (this *WithdrawParam) Deserialize(rd io.Reader) error {
-	var err error
-	if this.ContractAddr, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Initiator, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Delegate, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Role, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	return nil
-}
-
-type VerifyTokenParam struct {
-	ContractAddr []byte
-	Caller       []byte
-	Fn           []byte
-}
-
-func (this *VerifyTokenParam) Serialize(w io.Writer) error {
-	if err := serialization.WriteVarBytes(w, this.ContractAddr); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Caller); err != nil {
-		return err
-	}
-	if err := serialization.WriteVarBytes(w, this.Fn); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (this *VerifyTokenParam) Deserialize(rd io.Reader) error {
-	var err error
-	if this.ContractAddr, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
-	}
-	if this.Caller, err = serialization.ReadVarBytes(rd); err != nil {
-		return err //deserialize caller error
-	}
-	if this.Fn, err = serialization.ReadVarBytes(rd); err != nil {
-		return err
+		this.funcNames = append(this.funcNames, fn)
 	}
 	return nil
 }
 
 type AuthToken struct {
+	role       []byte
 	expireTime uint32
-	level      int
+	level      uint8
 }
 
 func (this *AuthToken) Serialize(w io.Writer) error {
-	//bf := new(bytes.Buffer)
-	if err := serialization.WriteVarUint(w, uint64(this.expireTime)); err != nil {
+	if err := serialization.WriteVarBytes(w, this.role); err != nil {
 		return err
 	}
-	if err := serialization.WriteVarUint(w, uint64(this.level)); err != nil {
+	if err := serialization.WriteUint32(w, this.expireTime); err != nil {
+		return err
+	}
+	if err := serialization.WriteUint8(w, this.level); err != nil {
 		return err
 	}
 	return nil
@@ -317,29 +81,109 @@ func (this *AuthToken) Serialize(w io.Writer) error {
 
 func (this *AuthToken) Deserialize(rd io.Reader) error {
 	//rd := bytes.NewReader(data)
-	expireTime, err := serialization.ReadVarUint(rd, 0)
+	var err error
+	this.role, err = serialization.ReadVarBytes(rd)
 	if err != nil {
 		return err
 	}
-	level, err := serialization.ReadVarUint(rd, 0)
+	this.expireTime, err = serialization.ReadUint32(rd)
 	if err != nil {
 		return err
 	}
-	this.expireTime = uint32(expireTime)
-	this.level = int(level)
+	this.level, err = serialization.ReadUint8(rd)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (this *AuthToken) serialize() ([]byte, error) {
-	bf := new(bytes.Buffer)
-	err := this.Serialize(bf)
-	if err != nil {
-		return nil, err
-	}
-	return bf.Bytes(), nil
+type DelegateStatus struct {
+	root []byte
+	AuthToken
 }
 
-func (this *AuthToken) deserialize(data []byte) error {
-	rd := bytes.NewReader(data)
-	return this.Deserialize(rd)
+func (this *DelegateStatus) Serialize(w io.Writer) error {
+	if err := serialization.WriteVarBytes(w, this.root); err != nil {
+		return err
+	}
+	if err := this.AuthToken.Serialize(w); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *DelegateStatus) Deserialize(rd io.Reader) error {
+	var err error
+	this.root, err = serialization.ReadVarBytes(rd)
+	if err != nil {
+		return err
+	}
+	err = this.AuthToken.Deserialize(rd)
+	return err
+}
+
+type Status struct {
+	status []*DelegateStatus
+}
+
+func (this *Status) Serialize(w io.Writer) error {
+	if err := serialization.WriteUint32(w, uint32(len(this.status))); err != nil {
+		return err
+	}
+	for _, s := range this.status {
+		if err := s.Serialize(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (this *Status) Deserialize(rd io.Reader) error {
+	sLen, err := serialization.ReadUint32(rd)
+	if err != nil {
+		return err
+	}
+	this.status = make([]*DelegateStatus, 0)
+	for i := uint32(0); i < sLen; i++ {
+		s := new(DelegateStatus)
+		err = s.Deserialize(rd)
+		if err != nil {
+			return err
+		}
+		this.status = append(this.status, s)
+	}
+	return nil
+}
+
+type roleTokens struct {
+	tokens []*AuthToken
+}
+
+func (this *roleTokens) Serialize(w io.Writer) error {
+	if err := serialization.WriteUint32(w, uint32(len(this.tokens))); err != nil {
+		return err
+	}
+	for _, token := range this.tokens {
+		if err := token.Serialize(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (this *roleTokens) Deserialize(rd io.Reader) error {
+	tLen, err := serialization.ReadUint32(rd)
+	if err != nil {
+		return err
+	}
+	this.tokens = make([]*AuthToken, 0)
+	for i := uint32(0); i < tLen; i++ {
+		tok := new(AuthToken)
+		err = tok.Deserialize(rd)
+		if err != nil {
+			return err
+		}
+		this.tokens = append(this.tokens, tok)
+	}
+	return nil
 }
