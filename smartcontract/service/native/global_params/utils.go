6c9cd1dcc655d61bedfd0eac77409e373256f448
JhoneLee6 <JhoneLee6@gmail.com>
2021-01-11 15:52:28 +0800

    add events notify in global parmas contract (#480)

--- smartcontract/service/native/global_params/utils.go
+++ smartcontract/service/native/global_params/utils.go
@@ -22,7 +22,9 @@ import (
 	"bytes"
 
 	"github.com/ontio/dad-go/common"
+	"github.com/ontio/dad-go/common/config"
 	cstates "github.com/ontio/dad-go/core/states"
+	"github.com/ontio/dad-go/smartcontract/event"
 	"github.com/ontio/dad-go/smartcontract/service/native"
 	"github.com/ontio/dad-go/smartcontract/service/native/utils"
 )
@@ -87,4 +89,44 @@ func GetStorageRole(native *native.NativeService, key []byte) (common.Address, e
 	bf := bytes.NewBuffer(item.Value)
 	role, err = utils.ReadAddress(bf)
 	return role, err
+}
+
+func NotifyRoleChange(native *native.NativeService, contract common.Address, functionName string,
+	newAddr common.Address) {
+	if !config.DefConfig.Common.EnableEventLog {
+		return
+	}
+	native.Notifications = append(native.Notifications,
+		&event.NotifyEventInfo{
+			ContractAddress: contract,
+			States:          []interface{}{functionName, newAddr.ToBase58()},
+		})
+}
+
+func NotifyTransferAdmin(native *native.NativeService, contract common.Address, functionName string,
+	originAdmin, newAdmin common.Address) {
+	if !config.DefConfig.Common.EnableEventLog {
+		return
+	}
+	native.Notifications = append(native.Notifications,
+		&event.NotifyEventInfo{
+			ContractAddress: contract,
+			States:          []interface{}{functionName, originAdmin.ToBase58(), newAdmin.ToBase58()},
+		})
+}
+
+func NotifyParamChange(native *native.NativeService, contract common.Address, functionName string, params *Params) {
+	if !config.DefConfig.Common.EnableEventLog {
+		return
+	}
+	paramsString := ""
+	for _, param := range *params {
+		paramsString += param.Key + "," + param.Value + ";"
+	}
+	paramsString = paramsString[:len(paramsString)-1]
+	native.Notifications = append(native.Notifications,
+		&event.NotifyEventInfo{
+			ContractAddress: contract,
+			States:          []interface{}{functionName, paramsString},
+		})
 }
