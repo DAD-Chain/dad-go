c71514d9859b7b405c7bbbe51913367b63bcfb30
Fungus-Light <542461670@qq.com>
2021-11-18 08:26:25 +0800

    clear dualportsupport code (#893)
    * clear dualportsupport code
    
    * clear consport
    
    * clear consState
    
    * go fmt
    
    * clear recvsyncchan
    
    * clear isConsus
    
    * rename syncChan to RecvChan
    
    * rename sync
    
    * rename  Sync
    
    * rename getsyncport
    
    * rename removepeersyncaddress
    
    * rename syncchan
    
    * fix nbr_peer_test bug
    
    * fix msg_handler_test bug
    
    * add license
    
    * go fmt
    
    * code clean

--- smartcontract/service/native/ontid/owner.go
+++ smartcontract/service/native/ontid/owner.go
@@ -1,4 +1,6 @@
-/* This file is part of The dad-go library.
+/*
+ * Copyright (C) 2018 The dad-go Authors
+ * This file is part of The dad-go library.
  *
  * The dad-go is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
